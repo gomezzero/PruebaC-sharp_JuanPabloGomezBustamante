8e24f9f00857e0ffa889ff6917c32b3de6a663fb
gomez-zero <gomezjuanzero@gmail.com>
2024-08-05 06:39:56 -0500

    feat: agregando carpeta Models y las clases principales vacias

---
+++ Models/Dog.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PruebaC_sharp_JuanPabloGomezBustamante.Models
+{
+    public class Dog
+    {
+        
+    }
+}
